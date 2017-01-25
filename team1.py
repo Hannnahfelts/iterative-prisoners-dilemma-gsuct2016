907b11f309faa6554f44df259143854a251b7aa0
Hannnahfelts <024879@daltonK12.onmicrosoft.com>
2017-01-25 10:38:19 -0500

    Team 1 names
    Hannah Felts 
    Team Catamounts

--- team1.py
+++ team1.py
@@ -3,10 +3,9 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
+#     move: A function that ret' # Only 10 chars displayed.
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Catamounts' # only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
